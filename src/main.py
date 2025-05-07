dbb275a9b2eb5ea57c642e33d45eae9f2fe71dbc
Huy T. Vo <hvo@stone.local>
2025-05-07 10:55:53 -0400

    Added the boilerplate for FastAPI on Google App Engine

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, JSONResponse
+
+
+app = FastAPI(title='CSC 221 - FastAPI on Google App Engine',
+              description='The boilerplate for a FastAPI app on Google App Engine',
+              redoc_url=None,
+              )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_headers=['*'],
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'], # allow_methods=['*'],
+)
+
+@app.exception_handler(Exception)
+async def validation_exception_handler(request, err):
+    return JSONResponse(status_code=500, content={
+        'detail': [
+            {'msg': str(err), 'type': type(err).__name__}
+        ]
+    })
+
+router = APIRouter()
+
+@router.get('/')
+async def home_page():
+    return FileResponse('index.html')
+
+app.include_router(router)
---
+++ src/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from .main import app
+import os
+
+client = TestClient(app)
+
+
+def test_home_page():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert len(response.text) == os.path.getsize('index.html')
